2add05ba57106df79fc9eed4ed62463af1448750
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-03-06 18:40:00 +0100

    Update ItemInit.java

--- src/main/java/com/turtysproductions/humlands/common/init/ItemInit.java
+++ src/main/java/com/turtysproductions/humlands/common/init/ItemInit.java
@@ -25,5 +25,11 @@ public class ItemInit {
 	
 	public static final RegistryObject<Item> SUPER_ROYAL_SLIME = ITEMS.register("super_royal_slime", () -> new GlintingItem(new Item.Properties().group(HumlandsTab.HUMLANDS).rarity(Rarity.EPIC)));
 	public static final RegistryObject<Item> SUPER_ROYAL_FLAN = ITEMS.register("super_royal_flan", () -> new GlintingItem(new Item.Properties().group(HumlandsTab.HUMLANDS).rarity(Rarity.EPIC).food(new Food.Builder().hunger(8).saturation(4.0f).effect(new EffectInstance(Effects.SPEED, 6000, 5), 1).effect(new EffectInstance(Effects.HEALTH_BOOST), 1).build())));
-	public static final RegistryObject<Item> SUPER_ROYAL_FLAN_RAW = ITEMS.register("super_royal_flan_raw", () -> new GlintingItem(new Item.Properties().group(HumlandsTab.HUMLANDS).rarity(Rarity.EPIC)));
+        public static final RegistryObject<Item> SUPER_ROYAL_FLAN_RAW = ITEMS.register("super_royal_flan_raw", () -> new GlintingItem(new Item.Properties().group(HumlandsTab.HUMLANDS).rarity(Rarity.EPIC)));
+	
+	public static final RegistryObject<Item> flux_vial = ITEMS.register("flux_vial", () -> new FluxVial(() -> Fluids.EMPTY, new Item.Properties().group(ItemGroup.MISC).maxStackSize(1)));
+        public static RegistryObject<Item> full_vial = ITEMS.register("full_vial", () -> new FluxVial(FluidInit.flux_fluid, new Item.Properties().containerItem(ItemInit.flux_vial.get()).maxStackSize(1).group(ItemGroup.MISC)));
+	
+	
+	
 }
