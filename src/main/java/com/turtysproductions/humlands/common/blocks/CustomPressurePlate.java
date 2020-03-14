c26b40217761bf330b2aacb2b09cbd97b857c73e
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-03-14 21:26:51 +0100

    Create CustomPressurePlate.java

---
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomPressurePlate.java
@@ -0,0 +1,11 @@
+package com.turtysproductions.humlands.common.blocks;
+import net.minecraft.block.PressurePlateBlock;
+public class CustomPressurePlate extends PressurePlateBlock
+{
+
+	public CustomPressurePlate(Sensitivity sensitivityIn, Properties propertiesIn) {
+		super(sensitivityIn, propertiesIn);
+		
+	}
+
+}
