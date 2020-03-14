c4200f053486dd240aa63a697fc112fa6deebf2a
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-03-14 21:25:39 +0100

    Create CustomTrapdoor.java

---
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomTrapdoor.java
@@ -0,0 +1,12 @@
+package com.turtysproductions.humlands.common.blocks;
+
+import net.minecraft.block.TrapDoorBlock;
+
+public class CustomTrapdoor extends TrapDoorBlock  {
+
+	public CustomTrapdoor(Properties properties) {
+		super(properties);
+		
+	}
+
+}
