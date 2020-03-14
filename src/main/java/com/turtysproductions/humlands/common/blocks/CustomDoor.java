08730cc01ef17fe4843f8ebe7607f6adb5bcf764
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-03-14 21:22:25 +0100

    Create CustomDoor.java

---
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomDoor.java
@@ -0,0 +1,13 @@
+package com.turtysproductions.humlands.common.blocks;
+
+import net.minecraft.block.DoorBlock;
+
+public class CustomDoor extends DoorBlock
+{
+
+	public CustomDoor(Properties builder)
+	{
+		super(builder);
+	}
+
+}
