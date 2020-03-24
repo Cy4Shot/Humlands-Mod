2b01c36b8aba5fee8785b85003dd9e56541c7bf9
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-03-24 11:40:35 +0100

    Create CustomGrass.java

---
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomGrass.java
@@ -0,0 +1,13 @@
+package com.turtysproductions.humlands.common.blocks;
+
+import net.minecraft.block.GrassBlock;
+
+public class CustomGrass extends GrassBlock
+{
+
+	public CustomGrass(Properties properties) {
+		super(properties);
+		
+	}
+
+}
