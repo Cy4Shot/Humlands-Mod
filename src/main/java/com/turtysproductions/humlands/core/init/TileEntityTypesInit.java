d445fb8fd0c11cc35d1a5b789d5342d8805cd23b
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-04-10 22:40:49 +0200

    Create TileEntityTypesInit.java

---
+++ src/main/java/com/turtysproductions/humlands/core/init/TileEntityTypesInit.java
@@ -0,0 +1,17 @@
+package com.turtysproductions.humlands.core.init;
+
+import com.turtysproductions.humlands.HumlandsMod;
+import com.turtysproductions.humlands.common.tileentities.TimeCapsuleTileEntity;
+
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TileEntityTypesInit 
+{
+	public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, HumlandsMod.MOD_ID);
+
+	public static final RegistryObject<TileEntityType<TimeCapsuleTileEntity>> TIME_CAPSULE_TILE_ENTITY = TILE_ENTITY_TYPES
+	.register("time_capsule_tile_entity", () -> TileEntityType.Builder.create(TimeCapsuleTileEntity::new, BlockInit.TIME_CAPSULE.get()).build(null));
+}
