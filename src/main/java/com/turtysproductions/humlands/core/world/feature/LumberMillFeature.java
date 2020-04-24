1123547d09886c29d02b128adcf96c070611a143
Cy4Shot <veschi@gmail.com>
2020-04-24 19:26:55 +0100

    Fixed processors for structure spawning

--- src/main/java/com/turtysproductions/humlands/core/world/feature/LumberMillFeature.java
+++ src/main/java/com/turtysproductions/humlands/core/world/feature/LumberMillFeature.java
@@ -17,7 +17,7 @@ import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
-import net.minecraft.world.gen.feature.template.IntegrityProcessor;
+import net.minecraft.world.gen.feature.template.GravityStructureProcessor;
 import net.minecraft.world.gen.feature.template.PlacementSettings;
 import net.minecraft.world.gen.feature.template.Template;
 import net.minecraft.world.server.ServerWorld;
@@ -40,14 +40,6 @@ public class LumberMillFeature extends Feature<NoFeatureConfig> {
 		Rotation rotation = arotation[random.nextInt(arotation.length)];
 		Template template = ((ServerWorld) worldIn.getWorld()).getSaveHandler().getStructureTemplateManager()
 				.getTemplateDefaulted(LUMBER_MILLS[random.nextInt(LUMBER_MILLS.length)]);
-
-		PlacementSettings placementsettings = (new PlacementSettings()).setRotation(rotation)
-				// .setBoundingBox defines until where the structure should spawn, for example
-				// here it's max 25600 X, 256 Y and 25600 Z
-				.setBoundingBox(new MutableBoundingBox((0 - 25600), (0 - 25600), (0 - 25600), 25600, 256, 25600))
-				.setRandom(random).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
-		// j k l are respectively x y z , and define the width, height and depth of the
-		// structure
 		BlockPos blockpos = template.transformedSize(rotation);
 		int j = random.nextInt(32 - 0);
 		int k = random.nextInt(32 - 0);
@@ -57,13 +49,15 @@ public class LumberMillFeature extends Feature<NoFeatureConfig> {
 				l = Math.min(l, worldIn.getHeight(Heightmap.Type.WORLD_SURFACE_WG, pos.getX(), pos.getZ()));
 			}
 		}
-		// This is what actually generates the structure
-		BlockPos blockpos1 = template.getZeroPositionWithTransform(
-				new BlockPos(pos.add(j, 0, k).getX(), l, pos.add(j, 0, k).getZ()), Mirror.NONE, rotation);
-		IntegrityProcessor integrityprocessor = new IntegrityProcessor(0.9F);
-		placementsettings.clearProcessors().addProcessor(integrityprocessor);
-		template.addBlocksToWorld(worldIn, blockpos1, placementsettings, 4);
-		placementsettings.func_215220_b(integrityprocessor);
+		template.addBlocksToWorld(worldIn,
+				template.getZeroPositionWithTransform(new BlockPos(pos.add(j, 0, k).getX(), l, pos.add(j, 0, k).getZ()),
+						Mirror.NONE, rotation),
+				(new PlacementSettings()).setRotation(rotation)
+						.setBoundingBox(
+								new MutableBoundingBox((0 - 25600), (0 - 25600), (0 - 25600), 25600, 256, 25600))
+						.setRandom(random).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK)
+						.addProcessor(new GravityStructureProcessor(Heightmap.Type.WORLD_SURFACE_WG, 0)),
+				4);
 		return true;
 
 	}
