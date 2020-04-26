67eed186be5cec81006af1566e51cb7c1e2a97ef
Cy4Shot <veschi@gmail.com>
2020-04-26 18:35:26 +0100

    Create LootTableInit.java

---
+++ src/main/java/com/turtysproductions/humlands/core/init/LootTableInit.java
@@ -0,0 +1,9 @@
+package com.turtysproductions.humlands.core.init;
+
+import com.turtysproductions.humlands.HumlandsMod;
+
+import net.minecraft.util.ResourceLocation;
+
+public class LootTableInit {
+	public static final ResourceLocation MOB_ROOM_CHEST = new ResourceLocation(HumlandsMod.MOD_ID, "chests/mob_room");
+}
