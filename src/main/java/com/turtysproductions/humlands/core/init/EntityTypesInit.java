90bdb706ab48b792727a1176e72fbb8de2aff2b5
MrShawshank <61604648+MrShawshank@users.noreply.github.com>
2020-05-02 09:34:09 +0200

    Update EntityTypesInit.java

--- src/main/java/com/turtysproductions/humlands/core/init/EntityTypesInit.java
+++ src/main/java/com/turtysproductions/humlands/core/init/EntityTypesInit.java
@@ -11,10 +11,24 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class EntityTypesInit {
-	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = new DeferredRegister<>(ForgeRegistries.ENTITIES,
-			HumlandsMod.MOD_ID);
+	public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = new DeferredRegister<>(ForgeRegistries.ENTITIES, HumlandsMod.MOD_ID);
 
-	public static final RegistryObject<EntityType<Humadillo>> HUMADILLO = ENTITY_TYPES.register("humadillo",
-			() -> EntityType.Builder.<Humadillo>create(Humadillo::new, EntityClassification.CREATURE).size(0.9f, 1.3f)
-					.build(new ResourceLocation(HumlandsMod.MOD_ID, "humadillo").toString()));
+	public static final RegistryObject<EntityType<Humadillo>> HUMADILLO = ENTITY_TYPES
+	.register("humadillo", () -> EntityType.Builder.<Humadillo>create(Humadillo::new, EntityClassification.CREATURE)
+	.size(0.9f, 1.3f)
+	.build(new ResourceLocation(HumlandsMod.MOD_ID, "humadillo").toString()));
+	
+	public static final RegistryObject<EntityType<Humahog>> HUMAHOG = ENTITY_TYPES
+        .register("humahog", () -> EntityType.Builder.<Humahog>create(Humahog::new, EntityClassification.MONSTER)
+        .size(0.9f, 1.3f)
+        .build(new ResourceLocation(HumlandsMod.MOD_ID, "humahog").toString()));
+        
+        public static final RegistryObject<EntityType<RubberBallEntity>> RUBBER_BALL_ENTITY = ENTITY_TYPES
+        .register("rubber_ball_entity", () -> EntityType.Builder.<RubberBallEntity>create(RubberBallEntity::new, EntityClassification.MISC)
+        .size(0.9f, 1.3f)
+        .build(new ResourceLocation(HumlandsMod.MOD_ID, "rubber_ball_entity").toString()));
+	
+	
+	
+	
 }
