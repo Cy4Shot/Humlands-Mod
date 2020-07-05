5f41e75038b2e2e2e7c0604c23a4a3a40f0aa78e
Shuricks <tbb.shuricks@gmail.com>
2020-07-05 20:00:40 +0200

    BugFix
    Fix Fire on Rubberwood

--- src/main/java/com/turtysproductions/humlands/common/blocks/CustomSlab.java
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomSlab.java
@@ -1,11 +1,25 @@
 package com.turtysproductions.humlands.common.blocks;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
 public class CustomSlab extends SlabBlock {
 
 	public CustomSlab(Properties properties) {
 		super(properties);
 
+	}
+	
+	@Override
+	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face){
+		return 20;
+	}
+	
+	@Override
+	public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face){
+		return 5;
 	}
 }
--- src/main/java/com/turtysproductions/humlands/common/blocks/CustomStairs.java
+++ src/main/java/com/turtysproductions/humlands/common/blocks/CustomStairs.java
@@ -2,11 +2,24 @@ package com.turtysproductions.humlands.common.blocks;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
 public class CustomStairs extends StairsBlock {
 
 	@SuppressWarnings("deprecation")
 	public CustomStairs(BlockState state, Properties properties) {
 		super(state, properties);
+	}
+	
+	@Override
+	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face){
+		return 20;
+	}
+	
+	@Override
+	public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face){
+		return 5;
 	}
 }
