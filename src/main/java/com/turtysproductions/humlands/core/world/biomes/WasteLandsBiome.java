3baeaf7fc09985412662e3c326f4993d512fd243
Cy4Shot <veschi@gmail.com>
2020-10-06 22:56:08 +0100

    Im back! (World Gen and Flux Extractor)

--- src/main/java/com/turtysproductions/humlands/core/world/biomes/WasteLandsBiome.java
+++ src/main/java/com/turtysproductions/humlands/core/world/biomes/WasteLandsBiome.java
@@ -1,16 +1,20 @@
 package com.turtysproductions.humlands.core.world.biomes;
 
+import com.turtysproductions.humlands.core.init.BlockInit;
 import com.turtysproductions.humlands.core.world.gen.ModBiomeFeatures;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
 
 public class WasteLandsBiome extends HumlandsBaseBiome {
 
 	public WasteLandsBiome(Builder biomeBuilder) {
 		super(biomeBuilder);
-		ModBiomeFeatures.addPonds(this, 80);
 		ModBiomeFeatures.addDeadTrees(this, 1);
 		DefaultBiomeFeatures.addGrass(this);
+		ModBiomeFeatures.addCustomRocks(this, Blocks.MOSSY_COBBLESTONE.getDefaultState(), 5);
+		ModBiomeFeatures.addCustomRocks(this, BlockInit.SCRAP_SAND.get().getDefaultState(), 5);
+		ModBiomeFeatures.addOreClump(this, BlockInit.FLUX_ORE_CLUMP.get().getDefaultState(), 6);
 	}
 	
 	@Override
